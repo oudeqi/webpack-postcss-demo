3164a3ebf74a818df78df11af6026497a3de0948
oudeqi <496997374@qq.com>
2017-10-03 01:08:45 +0800

    **

--- postcss.config.js
+++ postcss.config.js
@@ -27,7 +27,7 @@ module.exports = {
 		require('postcss-functions')({
 			functions: {
 		        px2rem: function (int) {
-		        	return parseFloat(int / 32) + 'rem';
+		        	return parseFloat(parseInt(int) / 32) + 'rem';
 		        }
 		    }
 		}),//自定义函数 px2rem
